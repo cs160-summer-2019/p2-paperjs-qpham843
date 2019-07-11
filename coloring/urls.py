7aa94ac5b389e351f59fd8fdd27a8e36b6783b7a
qpham843 <qpham843@berkeley.edu>
2019-07-11 01:14:09 -0400

    Added a prototype and bars pages to test functionality.

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('prototype/', views.prototype, name='prototype'),
+    path('bars/', views.bars, name='bars'),
     path('', views.index, name='index')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+  
+def prototype(request):
+    return render(request, 'coloring/prototype.html')
+  
+def bars(request):
+    return render(request, 'coloring/bars.html')
